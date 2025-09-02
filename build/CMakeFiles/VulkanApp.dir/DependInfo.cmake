9caf9ab786d23cedbc171305658e4baebfc78c07
tj894 <tj8948@bruh.com>
2025-09-02 10:18:23 -0400

    Lab work Computer graphics

---
+++ build/CMakeFiles/VulkanApp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/first_app.cpp" "CMakeFiles/VulkanApp.dir/first_app.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/first_app.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/lve_device.cpp" "CMakeFiles/VulkanApp.dir/lve_device.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/lve_device.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/lve_model.cpp" "CMakeFiles/VulkanApp.dir/lve_model.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/lve_model.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/lve_pipeline.cpp" "CMakeFiles/VulkanApp.dir/lve_pipeline.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/lve_pipeline.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/lve_swap_chain.cpp" "CMakeFiles/VulkanApp.dir/lve_swap_chain.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/lve_swap_chain.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/lve_window.cpp" "CMakeFiles/VulkanApp.dir/lve_window.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/lve_window.cpp.o.d"
+  "/Users/daewit/Desktop/Computer Graphics/TutorialRepo/littleVulkanEngine/littleVulkanEngine/tutorial08/main.cpp" "CMakeFiles/VulkanApp.dir/main.cpp.o" "gcc" "CMakeFiles/VulkanApp.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VulkanApp.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanApp.dir/first_app.cpp.o"
+  "CMakeFiles/VulkanApp.dir/first_app.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/lve_device.cpp.o"
+  "CMakeFiles/VulkanApp.dir/lve_device.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/lve_model.cpp.o"
+  "CMakeFiles/VulkanApp.dir/lve_model.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/lve_pipeline.cpp.o"
+  "CMakeFiles/VulkanApp.dir/lve_pipeline.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/lve_swap_chain.cpp.o"
+  "CMakeFiles/VulkanApp.dir/lve_swap_chain.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/lve_window.cpp.o"
+  "CMakeFiles/VulkanApp.dir/lve_window.cpp.o.d"
+  "CMakeFiles/VulkanApp.dir/main.cpp.o"
+  "CMakeFiles/VulkanApp.dir/main.cpp.o.d"
+  "VulkanApp"
+  "VulkanApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
